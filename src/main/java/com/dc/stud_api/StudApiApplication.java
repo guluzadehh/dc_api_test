400a9ac3b0f866abce1b0c6a8dac7b4459fb606e
guluzadehh <quluzade.resul13@mail.ru>
2024-11-28 02:14:18 +0400

    Init Spring

---
+++ src/main/java/com/dc/stud_api/StudApiApplication.java
@@ -0,0 +1,13 @@
+package com.dc.stud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudApiApplication.class, args);
+	}
+
+}
